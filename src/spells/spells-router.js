a56f5a4c97d0a1d7d1c1f96b2e301fae3596954f
Ron Martin <ronmartin89@gmail.com>
2020-03-11 13:29:24 -0700

    Completed the POST request in ROUTE, need to finish the knex service for insert

--- src/spells/spells-router.js
+++ src/spells/spells-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const xss = require('xss');
+const path = require('path');
 
 const SpellsService = require('./spells-service');
 
@@ -11,12 +12,12 @@ const serializeSpell = spell => ({
   spell_name: xss(spell.spell_name),
   level: spell.level,
   spell_school: xss(spell.spell_school),
-  spell_range: xss(spell.spell_range),
+  spell_range: xss(spell.spell_range) || '', // optional field
   cast_time: xss(spell.cast_time),
-  spell_components: xss(spell.spell_components), // do I need to add a " || ''" or " || {}" to entries that can be NULL?
+  spell_components: xss(spell.spell_components) || '', // optional field
   spell_duration: xss(spell.spell_duration),
   spell_description: xss(spell.spell_description),
-  higher_levels: xss(spell.higher_levels)
+  higher_levels: xss(spell.higher_levels) || '' // optional field
 });
 
 spellsRouter
@@ -29,11 +30,10 @@ spellsRouter
         res.json(spells.map(serializeSpell));
       })
       .catch(next);
-
-    // HOW DO I WRITE THIS WITH ASYNC AWAIT?
-    // Where do I put the 'async' ??
   })
   .post(jsonParser, (req, res, next) => {
+    // OPTIONAL FIELDS: spell_range, spell_components, higher_levels
+
     // get all fields that will be used at all 
     // in this case we dont take an ID because the server will be creating that - we dont POST that
     const { spell_name,
@@ -48,11 +48,66 @@ spellsRouter
     // make a newSpell object that we'll add values to after validation
     // first only adding the REQUIRED fields
     const newSpell = { spell_name, spell_school, cast_time, spell_duration, spell_description };
-
-    //VALIDATION HERE
     
-    // add back values to the newSpell object that werent need for validation or required fields
-    // ie newSpell.spell_range = spell_range;
+    // When I make the input DROPDOWNs for certain values, using things like this to validate against
+    //  const validSpellSchools = ['Evocation', 'Illusion', 'etc'];
+    //  const validCastTimes = ['1 Action', 'Forever lol'];
+
+    // VALIDATION HERE
+    // Validating to make sure that all the required fields are in the request
+    for (const [key, value] of Object.entries(newSpell)) {
+      if (value == null) { // eslint-disable-line eqeqeq
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
+      }
+    }
+    
+    if(isNaN(spell_level))  {
+      return res.status(400).json({
+        error: `'spell_level' must be an integer number`
+      });
+    }
+    
+    const assumedStrings = { 
+      spell_name, 
+      spell_school, 
+      spell_range, 
+      cast_time, 
+      spell_components, 
+      spell_duration, 
+      spell_description, 
+      higher_levels
+    };
+
+    // Validating that all values are strings that are supposed to be
+    for (const [key, value] of Object.entries(assumedStrings)) {
+      // Make sure we don't send a 400 if it was an optional field is empty. Required fields have already been tested.
+      if(value != null && typeof(value) !== 'string') { // eslint-disable-line eqeqeq
+        return res.status(400).json({
+          error: `'${key}' must be a string`
+        });
+      }
+    }
+
+
+    // Add optional values to newSpell after all validation is complete
+    newSpell.spell_range = spell_range;
+    newSpell.spell_components = spell_components;
+    newSpell.higher_levels = higher_levels;
+
+    // insert our data into the database
+    SpellsService.insertSpell(
+      req.app.get('db'),
+      newSpell
+    )
+      .then(spell => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${spell.id}`))
+          .json(serializeSpell(spell));
+      })
+      .catch(next);
   });
 
 module.exports = spellsRouter;
