0e179b7ad576ddc72dceac5efc4331b2ec04f5c4
Ron Martin <ronmartin89@gmail.com>
2020-03-11 22:08:46 -0700

    Still working on KNEX JOIN JOIN

--- src/classes/classes-service.js
+++ src/classes/classes-service.js
@@ -15,8 +15,15 @@ const ClassesService = {
       .from('spellbook_classes')
       .select('*')
       .join('spellbook_class_spells', 'spellbook_classes.id', '=', 'spellbook_class_spells.class_id')
+      // .join('spellbook_spells', 'spellbook_spell.id', '=', 'spellbook_class_spells.spell_id')
       .where('class_id', classId);
-     
+    // From KNEXJS DOCUMENTATION on Grouped Joins:
+    //
+    //   knex.select('*').from('users').join('accounts', function() {
+    //     this.on('accounts.id', '=', 'users.account_id').orOn('accounts.owner_id', '=', 'users.id')
+    //   })
+    //   Outputs:
+    //   select * from "users" inner join "accounts" on "accounts"."id" = "users"."account_id" or "accounts"."owner_id" = "users"."id"
   }
 };
 
